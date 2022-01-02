8114a0d93b55c60dafe68c5e4ace787dd88c7d2b
Muhammad Shahid Khan Afridi <muhammadshahidkhanafridi@gmail.com>
2022-01-02 19:58:08 +0500

    New Table in general created

---
+++ Procure.Database/Gen/Tables/Unit.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [Gen].[Unit]
+(
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [UnitCode]    NVARCHAR (50)  NOT NULL,
+    [UnitName]    NVARCHAR (100) NULL,
+    [Description] NVARCHAR (200) NULL,
+    [IsActive]    BIT            CONSTRAINT [DF_Gen_Unit_IsActive] DEFAULT ((1)) NULL,
+    [CreatedOn]   DATETIME       NULL,
+    [UpdatedOn]   DATETIME       NULL,
+    CONSTRAINT [PK_Gen_Unit] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [UC_Gen_UnitCode] UNIQUE NONCLUSTERED ([UnitCode] ASC)
+);
--- Procure.Database/Security/Inv.sql
+++ Procure.Database/Security/Inv.sql
@@ -1,3 +1,5 @@
 ﻿CREATE SCHEMA [Inv]
     AUTHORIZATION [dbo];
 
+
+
